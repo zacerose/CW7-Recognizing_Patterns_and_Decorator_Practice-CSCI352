008847d8b283f593b9b14a7e56cb20fb1a5007ec
Zachary Rose <zacerose@ut.utm.edu>
2023-02-14 14:05:21 -0600

    Decorator implementation started.

---
+++ Part 2/CW7-Decorator_Practice-CSCI352/CW7-Decorator_Practice-CSCI352/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW7_Decorator_Practice_CSCI352
+{
+    interface Widget
+    {
+        void draw();
+    }
+    class TextField : Widget
+    {
+        private int width, height;
+        public TextField(int w, int h)
+        {
+            width = w;
+            height = h;
+        }
+        public void draw()
+        {
+            Console.WriteLine("I am a text field, holding a: width, height");
+        }
+    }
+    abstract class Decorator : Widget
+    {
+        private Widget wid;
+        public Decorator(Widget w)
+        {
+            wid = w;
+        }
+        public void draw()
+        {
+            wid.draw();
+        }
+    }
+    class BorderDecorator : Decorator
+    {
+        public BorderDecorator(Widget w) : base(w)
+        {
+            
+        }
+        public void draw()
+        {
+            base.draw();
+            Console.WriteLine("I am a border decorator, holding a: ");
+        }
+    }
+    class ScrollDecorator : Decorator
+    {
+        public ScrollDecorator(Widget w) : base(w)
+        {
+
+        }
+        public void draw()
+        {
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
