6aaeb711a3d10bc717d9cd97417970c837dcae0d
Zachary Rose <zacerose@ut.utm.edu>
2023-02-15 11:25:23 -0600

    Fixed decorator behavior and added new decorator, the color decorator. Implemented main to construct a layered decorated text field.

--- Part 2/CW7-Decorator_Practice-CSCI352/CW7-Decorator_Practice-CSCI352/Program.cs
+++ Part 2/CW7-Decorator_Practice-CSCI352/CW7-Decorator_Practice-CSCI352/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Name: Zachary Rose
+// Date: 2/15/23
+// Class: CSCI352
+// Decorator practice example. Several decorators implemented that can be applied to a Widget. Main trivially applies and displays results from decorated draw() function.
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,7 +24,8 @@ namespace CW7_Decorator_Practice_CSCI352
         }
         public void draw()
         {
-            Console.WriteLine("I am a text field, holding a: width, height");
+            Console.WriteLine("I am a text field, width: " + width
+                + " and height: " + height);
         }
     }
     abstract class Decorator : Widget
@@ -30,7 +35,7 @@ namespace CW7_Decorator_Practice_CSCI352
         {
             wid = w;
         }
-        public void draw()
+        virtual public void draw()
         {
             wid.draw();
         }
@@ -41,10 +46,10 @@ namespace CW7_Decorator_Practice_CSCI352
         {
             
         }
-        public void draw()
+        override public void draw()
         {
             base.draw();
-            Console.WriteLine("border decorator ");
+            Console.WriteLine("Decorating border.");
         }
     }
     class ScrollDecorator : Decorator
@@ -53,16 +58,37 @@ namespace CW7_Decorator_Practice_CSCI352
         {
 
         }
-        public void draw()
+        override public void draw()
         {
             base.draw();
-            Console.WriteLine("scroll decorator ");
+            Console.WriteLine("Adding Scrollbar.");
+        }
+    }
+    class ColorDecorator : Decorator
+    {
+        private string color;
+
+        public ColorDecorator(Widget w, string color) : base(w)
+        {
+            this.color = color;
+        }
+        override public void draw()
+        {
+            base.draw();
+            Console.WriteLine("Coloring bar " + color +'.');
         }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
+            TextField field = new TextField(10, 20);
+            BorderDecorator decorator = new BorderDecorator(field);
+            ScrollDecorator scroll = new ScrollDecorator(decorator);
+            ColorDecorator fanciestTextbox = new ColorDecorator(scroll, "red");
+            fanciestTextbox.draw();
+
+            Console.ReadKey();
         }
     }
 }
